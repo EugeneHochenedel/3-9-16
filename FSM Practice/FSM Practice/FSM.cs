df92db8650433d3c4b50bae46e372f712469db52
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-03-15 09:01:06 -0500

    Populating and Changing
    Populating the dictionary and changing the state.

---
+++ FSM Practice/FSM Practice/FSM.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM_Practice
+{
+	//finite state machine
+	//Helps to determine how it'll be used ahead of time
+	//Might only need to be made once, then just reused
+	class FiniteStateMachine
+	{
+		public class Transition
+		{
+			public Enum Present;
+			public Enum Desired;
+			public Transition(Enum first, Enum second)
+			{
+				Present = first;
+				Desired = second;
+			}
+		}
+
+		Enum _currentState;
+		Dictionary<Enum, List<Transition>> _TransitionTable;
+		private List<Enum> _States;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="initial"></param>
+		public FiniteStateMachine(Enum initial)
+		{
+			_States = new List<Enum>();
+			_TransitionTable = new Dictionary<Enum, List<Transition>>();
+			_currentState = initial;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="state"></param>
+		/// <returns></returns>
+		public bool AddState(Enum state)
+		{
+			if (!_States.Contains(state))
+			{
+				_States.Add(state);
+				_TransitionTable.Add(state, new List<Transition>());
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("Can't do that.");
+				return false;
+			}
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void info()
+		{
+			Console.WriteLine("Finite State Machine is comprised of...");
+			foreach (Enum s in _States)
+			{
+				Console.WriteLine("State " + s.GetHashCode() + ": " + s.ToString());
+			}
+
+			Console.WriteLine("The current state is: " + _currentState.ToString());
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="transition">Must match the state->state format</param>
+		/// <returns>False if this is not a valid transition</returns>
+		public void AddTransition(Enum from, Enum to)
+		{
+			if (_TransitionTable.ContainsKey(from))
+			{
+				_TransitionTable[from].Add(new Transition(from, to));
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="change"></param>
+		public void ChangeState(Enum change)
+		{
+			Console.WriteLine("Change from " + _currentState.ToString() + " to " + change);
+
+			if (change.GetHashCode() > _currentState.GetHashCode() || change.GetHashCode() < _currentState.GetHashCode() && change.GetHashCode() != 0)
+			{
+				foreach (Transition t in _TransitionTable[_currentState])
+				{
+					if (t.Desired.Equals(change))
+					{
+						Console.WriteLine("Transitioning from: " + _currentState);
+						_currentState = change;
+						Console.WriteLine("To: " + _currentState);
+						Console.WriteLine("Current State: " + _currentState + "\n");
+					}
+				}
+			}
+			else if(change.GetHashCode() == 0 && _currentState.GetHashCode() > 0 || 
+					change.GetHashCode() == _currentState.GetHashCode())
+			{
+				Console.WriteLine("That transition is invalid! \nCurrent State:" + _currentState + "\n");	
+			}
+		}
+	}
+}
--- FSM Practice/FSM Practice/Program.cs
+++ FSM Practice/FSM Practice/Program.cs
@@ -6,100 +6,6 @@ using System.Threading.Tasks;
 
 namespace FSM_Practice
 {
-	//finite state machine
-	//Helps to determine how it'll be used ahead of time
-	//Might only need to be made once, then just reused
-	class FiniteStateMachine
-	{
-		public class Transition
-		{
-			public Enum Current;
-			public Enum Desired;
-			public Transition(Enum first, Enum second)
-			{
-				Current = first;
-				Desired = second;
-			}
-		//	public Enum from { get; set; }
-		}
-		
-		Enum _currentState;
-		public FiniteStateMachine(Enum initial)
-		{
-			_States = new List<Enum>();
-			_ValidTrans = new List<Enum>();
-			_currentState = initial;
-		}
-
-		private List<Enum> _States;
-
-		private List<Enum> _ValidTrans;
-
-		public bool AddState(Enum state)
-		{
-			//if (_States.Contains(state))
-			//{
-			//	Console.WriteLine("Can't do that.");
-			//	return false;
-			//}
-			if (!_States.Contains(state))
-			{
-				_States.Add(state);
-				return true;
-			}
-			else
-			{
-				Console.WriteLine("Can't do that.");
-				return false;
-			}
-
-		}
-
-		public void info()
-		{
-			Console.WriteLine("Finite State Machine is comprised of...");
-			//int iCount = 0;
-			foreach (Enum s in _States)
-			{
-				Console.WriteLine("State " + s.GetHashCode()/* iCount.ToString()*/ + ": " + s.ToString());
-				//iCount++;
-			}
-
-			Console.WriteLine("Current State is: " + _currentState.ToString());
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="transition">Must match the state->state format</param>
-		/// <returns>False if this is not a valid transition</returns>
-		public bool AddTransition(Enum from, Enum to)
-		{
-			Transition t = new Transition(from, to);
-			
-			List<Transition> Testing = new List<Transition>(4);
-			for (int iCount = 0; Testing.Capacity <= 4; iCount++)
-			{
-				Testing.Insert(0, t);
-			}
-			if (TransitionTable[from].Contains(t))
-			{
-				
-				Console.WriteLine("The active state is... " + from.ToString());
-				return true;
-			}
-			else// if(TransitionTable[from].Contains(t) == TransitionTable[to].Contains(t))
-			{
-				Console.WriteLine("You can't transition like that!");
-				return false;
-			}
-		}
-
-		Dictionary<Enum, List<Transition>> TransitionTable;
-
-
-	}
-
 	class Program
 	{
 		enum PlayerStates
@@ -121,14 +27,27 @@ namespace FSM_Practice
 			fsm.AddState(PlayerStates.idle);
 			fsm.AddState(PlayerStates.walk);
 			fsm.AddState(PlayerStates.run);
-			fsm.AddState(PlayerStates.init);
+			//fsm.AddState(PlayerStates.init);
+			fsm.AddTransition(PlayerStates.init, PlayerStates.idle);
+			fsm.AddTransition(PlayerStates.idle, PlayerStates.walk);
+			fsm.AddTransition(PlayerStates.walk, PlayerStates.run);
+			fsm.AddTransition(PlayerStates.run, PlayerStates.walk);
+			fsm.AddTransition(PlayerStates.walk, PlayerStates.idle);
 			//fsm.AddTransition(PlayerStates.init, PlayerStates.idle);
 			//fsm.AddTransition()
 			//need to know current state
 			//Pass on the constructor
+			//fsm.info();
+			fsm.ChangeState(PlayerStates.idle);
+			fsm.ChangeState(PlayerStates.init);
 
-			fsm.info();
+			//fsm.info();
 
+			fsm.ChangeState(PlayerStates.walk);
+			//fsm.info();
+			fsm.ChangeState(PlayerStates.idle);
+			fsm.ChangeState(PlayerStates.run);
+			
 			Console.ReadLine();
 		}
 	}
